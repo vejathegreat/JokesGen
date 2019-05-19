8feac2e3da07a6a6144fa6555c7878082c476ce6
veli <velly_mathebula@rocketmail.com>
2019-05-19 10:38:10 +0200

    Added Jokes

--- jokeslib/src/main/java/com/velaphi/jokeslib/JokesLib.java
+++ jokeslib/src/main/java/com/velaphi/jokeslib/JokesLib.java
@@ -3,24 +3,11 @@ package com.velaphi.jokeslib;
 public class JokesLib {
 
     private static String[] jokes = {
-            // Jokes Source: http://www.short-funny.com/
-            "Can a kangaroo jump higher than a house?" +"\n" + "Of course, a house doesn't jump at all",
-            "Anton, do you think I'm a bad mother?" + "\n" + "My name is Paul.",
+            "What do you get when you wake up on a workday and realize you ran out of coffee?" +"\n" + "A depresso.",
+            "I used to breed rabbits. Then I realized they can handle it themselves.",
             "My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away",
-            "What is the difference between a snowman and a snowwoman?" + "\n" + "Snowballs",
-            "My relationship is like an iPad. I don't have an iPad.",
-            "I am nobody. Nobody is perfect. I am perfect.",
-            "I'm not lazy. I'm just naturally a very relaxed person.",
-            "I'm standing outside. In other words, I'm outstanding.",
-            "Stressed is just desserts spelled backwards.",
-            "He who wakes up early, yawns all day long.",
-            "Dogs have beloved masters. Cats have waiting staff.",
-            "I've been single for so many years I believe I will soon become an album.",
-            "I had loads to do today. Ah well, so now I have loads to do tomorrow.",
-            "All my life I thought air was for free. That was until I bought a bag of crisps.",
-            "Of course you're not fat. Just grab a couple of chairs and come sit with us.",
-            "Where do pencils spend their vacations?" + "\n" + "In Pencilvania.",
-            "What did 0 say to 8?" + "\n" + "Hey, nice belt!"
+            "Why are eggs not very much into jokes? Because they could crack up.",
+            "I went to see the doctor about my short-term memory problems. The first thing the bastard did was made me pay in advance."
     };
 
     /**
